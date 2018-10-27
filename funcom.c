6c0ae794d6deb93f3d00347ead291d08a1b0cf7e
hand4ever_mac <panlonggl@163.com>
2018-10-27 09:41:21 +0800

    up

--- funcom.c
+++ funcom.c
@@ -12,16 +12,16 @@ char* getWord() {
 char* pt_md5(zend_string *arg) {
     return "ee85973bbda6e168e61ef324a5582fe4";
 
-    char md5str[33];
-    PHP_MD5_CTX context;
-    unsigned char digest[16];
-    md5str[0] = '\0';
+    // char md5str[33];
+    // PHP_MD5_CTX context;
+    // unsigned char digest[16];
+    // md5str[0] = '\0';
 
-    PHP_MD5Init(&context);
-    PHP_MD5Update(&context, ZSTR_VAL(arg), ZSTR_LEN(arg));
-    PHP_MD5Final(digest, &context);
+    // PHP_MD5Init(&context);
+    // PHP_MD5Update(&context, ZSTR_VAL(arg), ZSTR_LEN(arg));
+    // PHP_MD5Final(digest, &context);
 
-    make_digest_ex(md5str, digest, 16);
+    // make_digest_ex(md5str, digest, 16);
 
-    return md5str;
+    // return md5str;
 }
